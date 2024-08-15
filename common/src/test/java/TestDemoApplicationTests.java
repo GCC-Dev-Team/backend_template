6381349828c7d8fec5034a0f64731f8ded9e0c69
xiaoliverygood <ljzcomeon@gmail.com>
2024-08-15 23:05:55 +0800

    fear redis可选性

--- common/src/test/java/TestDemoApplicationTests.java
+++ common/src/test/java/TestDemoApplicationTests.java
@@ -6,7 +6,7 @@ import common.utils.cache.RedisCache;
 import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+
 
 
 @SpringBootTest(classes = CommonApplication.class)
